814792aa50bbc59800d67ddc03bad82667fdafab
pandepic <39251913+pandepic@users.noreply.github.com>
2019-08-17 21:33:15 +1000

    #

---
+++ frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PLEXRenamer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblFolderPath = new System.Windows.Forms.Label();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.dlgOpenFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblFolderPath
+            // 
+            this.lblFolderPath.AutoSize = true;
+            this.lblFolderPath.Location = new System.Drawing.Point(12, 9);
+            this.lblFolderPath.Name = "lblFolderPath";
+            this.lblFolderPath.Size = new System.Drawing.Size(61, 13);
+            this.lblFolderPath.TabIndex = 0;
+            this.lblFolderPath.Text = "Folder Path";
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(15, 25);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(291, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(312, 22);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(113, 23);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.BtnSelectFolder_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.lblFolderPath);
+            this.Name = "frmMain";
+            this.Text = "PLEX Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.Label lblFolderPath;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.FolderBrowserDialog dlgOpenFolder;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLEXRenamer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            txtFolderPath.Text = "D:\\PLEX Media";
+        }
+
+        private void BtnSelectFolder_Click(object sender, EventArgs e)
+        {
+            if (txtFolderPath.Text.Length > 0)
+            {
+                dlgOpenFolder.SelectedPath = txtFolderPath.Text;
+            }
+
+            var result = dlgOpenFolder.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txtFolderPath.Text = dlgOpenFolder.SelectedPath;
+            }
+        }
+    }
+}
