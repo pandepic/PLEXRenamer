b158fb8ef21115e8e3bc93d2582befa7d45a07f4
pandepic <39251913+pandepic@users.noreply.github.com>
2019-08-17 22:29:00 +1000

    #

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -33,6 +33,11 @@
             this.txtFolderPath = new System.Windows.Forms.TextBox();
             this.btnSelectFolder = new System.Windows.Forms.Button();
             this.dlgOpenFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtShowName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstEpisodes = new System.Windows.Forms.ListBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.prgRenaming = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // lblFolderPath
@@ -48,12 +53,12 @@
             // 
             this.txtFolderPath.Location = new System.Drawing.Point(15, 25);
             this.txtFolderPath.Name = "txtFolderPath";
-            this.txtFolderPath.Size = new System.Drawing.Size(291, 20);
+            this.txtFolderPath.Size = new System.Drawing.Size(410, 20);
             this.txtFolderPath.TabIndex = 1;
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(312, 22);
+            this.btnSelectFolder.Location = new System.Drawing.Point(431, 23);
             this.btnSelectFolder.Name = "btnSelectFolder";
             this.btnSelectFolder.Size = new System.Drawing.Size(113, 23);
             this.btnSelectFolder.TabIndex = 2;
@@ -61,11 +66,58 @@
             this.btnSelectFolder.UseVisualStyleBackColor = true;
             this.btnSelectFolder.Click += new System.EventHandler(this.BtnSelectFolder_Click);
             // 
+            // txtShowName
+            // 
+            this.txtShowName.Location = new System.Drawing.Point(15, 64);
+            this.txtShowName.Name = "txtShowName";
+            this.txtShowName.Size = new System.Drawing.Size(410, 20);
+            this.txtShowName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TV Show Name";
+            // 
+            // lstEpisodes
+            // 
+            this.lstEpisodes.FormattingEnabled = true;
+            this.lstEpisodes.HorizontalScrollbar = true;
+            this.lstEpisodes.Location = new System.Drawing.Point(15, 90);
+            this.lstEpisodes.Name = "lstEpisodes";
+            this.lstEpisodes.Size = new System.Drawing.Size(410, 420);
+            this.lstEpisodes.TabIndex = 5;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(431, 90);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(113, 23);
+            this.btnRename.TabIndex = 6;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.BtnRename_Click);
+            // 
+            // prgRenaming
+            // 
+            this.prgRenaming.Location = new System.Drawing.Point(431, 119);
+            this.prgRenaming.Name = "prgRenaming";
+            this.prgRenaming.Size = new System.Drawing.Size(113, 23);
+            this.prgRenaming.TabIndex = 7;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(556, 517);
+            this.Controls.Add(this.prgRenaming);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.lstEpisodes);
+            this.Controls.Add(this.txtShowName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSelectFolder);
             this.Controls.Add(this.txtFolderPath);
             this.Controls.Add(this.lblFolderPath);
@@ -83,6 +135,11 @@
         private System.Windows.Forms.TextBox txtFolderPath;
         private System.Windows.Forms.Button btnSelectFolder;
         private System.Windows.Forms.FolderBrowserDialog dlgOpenFolder;
+        private System.Windows.Forms.TextBox txtShowName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstEpisodes;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.ProgressBar prgRenaming;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace PLEXRenamer
 {
     public partial class frmMain : Form
     {
+        public List<Season> seasonList = new List<Season>();
+
         public frmMain()
         {
             InitializeComponent();
@@ -30,8 +33,90 @@ namespace PLEXRenamer
 
             if (result == DialogResult.OK)
             {
+                seasonList.Clear();
+                lstEpisodes.Items.Clear();
+
                 txtFolderPath.Text = dlgOpenFolder.SelectedPath;
+                txtShowName.Text = dlgOpenFolder.SelectedPath.Split('\\').Last();
+
+                var seasons = Directory.GetDirectories(txtFolderPath.Text);
+                int seasonNumber = 1;
+
+                foreach (var s in seasons)
+                {
+                    var episodes = Directory.GetFiles(s);
+                    var episodeNumber = 1;
+
+                    var newSeason = new Season()
+                    {
+                        name = "s" + (seasonNumber < 10 ? "0" : "") + seasonNumber.ToString(),
+                        path = s,
+                        number = seasonNumber,
+                        episodes = new List<Episode>(),
+                    };
+
+                    foreach (var ep in episodes)
+                    {
+                        newSeason.episodes.Add(new Episode()
+                        {
+                            name = "e" + (episodeNumber < 10 ? "0" : "") + episodeNumber.ToString(),
+                            path = ep,
+                            extension = ep.Split('.').Last(),
+                            number = episodeNumber,
+                        });
+
+                        episodeNumber += 1;
+                    }
+
+                    seasonList.Add(newSeason);
+                    seasonNumber += 1;
+                }
+
+                foreach (var s in seasonList)
+                {
+                    foreach (var ep in s.episodes)
+                    {
+                        lstEpisodes.Items.Add(s.name + ep.name + "." + ep.extension + " - " + ep.path);
+                    }
+                }
             }
         }
+
+        private void BtnRename_Click(object sender, EventArgs e)
+        {
+            prgRenaming.Maximum = lstEpisodes.Items.Count;
+            prgRenaming.Value = 0;
+            prgRenaming.Step = 1;
+
+            foreach (var s in seasonList)
+            {
+                foreach (var ep in s.episodes)
+                {
+                    var file = new FileInfo(ep.path);
+
+                    File.Move(file.FullName, file.Directory.FullName + "\\" + s.name + ep.name + "." + ep.extension);
+                    prgRenaming.PerformStep();
+                }
+            }
+
+            MessageBox.Show("Renaming Finished.");
+        }
+
+    }
+
+    public class Season
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public int number { get; set; }
+        public List<Episode> episodes { get; set; }
+    }
+
+    public class Episode
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public string extension { get; set; }
+        public int number { get; set; }
     }
 }
